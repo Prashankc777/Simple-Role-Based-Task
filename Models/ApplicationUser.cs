4ded49832f1b7004642cc4169324669be7de3dad
sushan.shrestha@aria.com.np <sushan.shrestha@aria.com.np>
2021-05-03 18:12:15 +0545

    Add project files.

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Krenova_tech.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
